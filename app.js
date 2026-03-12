0438623dbcd250238c2d0606992b956f5166b836
Md salim islam <salimislam0036@gmail.com>
2026-03-12 14:41:01 +0600

    feat(order): add order schema, order routes, SSLCommerz payment routes, and env configuration

--- app.js
+++ app.js
@@ -10,8 +10,11 @@ if (!fs.existsSync("./uploads")) {
     fs.mkdirSync("./uploads");
 }
 
-app.use(cors());
-
+// app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", "http://localhost:5174"],
+  credentials: true,
+}));
 
 app.use(express.json({ limit: '50mb' })); 
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
@@ -28,6 +31,8 @@ const userRoutes = require("./routes/user");
 const cartRoutes = require("./routes/cart");
 const reviewRoutes = require("./routes/productReviews");
 const myListRoutes = require("./routes/myList");
+const orderRoutes = require("./routes/orders");
+const paymentRoutes = require("./routes/payment");
 
 app.use("/api/category", categoryRoutes);
 app.use("/api/products", productRoutes);
@@ -40,6 +45,8 @@ app.use("/api/user", userRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/reviews", reviewRoutes);
 app.use("/api/myList", myListRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/payment", paymentRoutes);
 
 // Database connection
 mongoose
